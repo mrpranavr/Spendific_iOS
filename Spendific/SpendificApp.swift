6d90e321742e1da2b0371118940fbf0c6a74c4b0
Pranav R <pranav81234@gmail.com>
2024-06-25 14:32:02 +0530

    Initial Commit

---
+++ Spendific/SpendificApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendificApp.swift
+//  Spendific
+//
+//  Created by Pranav R on 25/06/24.
+//
+
+import SwiftUI
+
+@main
+struct SpendificApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
